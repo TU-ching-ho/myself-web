ee308d9cc7e8aa3aefd3c92a40cf68f7433f98fb
mark <gh850702@gmail.com>
2022-12-21 17:34:02 +0800

    圖片新增slider功能

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+$('.autoplay').slick({
+    dots: true,
+    infinite: true,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 1500,
+});
+
+$('.autoplay2').slick({
+    dots: true,
+    infinite: true,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 1500,
+});
